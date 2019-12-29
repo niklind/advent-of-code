9933dbf4e5578362aebeca49657182b326fe04ff
Niklas Lindblad <lindblad.n@gmail.com>
2019-12-30 00:05:21 +0100

    Fixed broken tests

--- 2019/day4/part2.py
+++ 2019/day4/part2.py
@@ -7,18 +7,17 @@ def parse_data(file: str):
 def find_valid_passwords(low, high):
     valid_passwords = []
     for password in range(low, high + 1):
-        numbers = [int(x) for x in str(password)]
-        if validate_password(numbers):
+        if validate_password(password):
             valid_passwords.append(password)
-    print(valid_passwords)
     return len(valid_passwords)
 
 
 def validate_password(password):
+    numbers = [int(x) for x in str(password)]
     previous = 0
     group = {}
 
-    for number in password:
+    for number in numbers:
         if number < previous:
             return False
         elif number == previous:
