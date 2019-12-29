21186b86e2f68adc06485508ca726130402dfdbd
Niklas Lindblad <lindblad.n@gmail.com>
2019-12-30 00:19:30 +0100

    Cleaned up stepping function a bit for day 3

--- 2019/day3/part2.py
+++ 2019/day3/part2.py
@@ -2,7 +2,7 @@ def parse_data(file: str):
     with open(file, 'r') as file:
         wire1 = list(file.readline().split(","))
         wire2 = list(file.readline().split(","))
-        return [wire1, wire2]
+        return wire1, wire2
 
 
 def manhattan_distance(wire1, wire2):
@@ -20,27 +20,34 @@ def calculate_path(wire):
     for vector in wire:
         direction = vector[0]
         length = int(vector[1:])
-        x_diff = 0
-        y_diff = 0
 
-        if direction == 'L':
-            x_diff = -1
-        elif direction == 'R':
-            x_diff = 1
-        elif direction == 'U':
-            y_diff = 1
-        elif direction == 'D':
-            y_diff = -1
+        x_step, y_step = step(direction)
 
         for _ in range(0, length):
-            x += x_diff
-            y += y_diff
+            x += x_step
+            y += y_step
             steps += 1
 
             if(x, y) not in visited_points:
                 visited_points[(x, y)] = steps
 
     return visited_points
+
+
+def step(direction):
+    x_diff = 0
+    y_diff = 0
+
+    if direction == 'L':
+        x_diff = -1
+    elif direction == 'R':
+        x_diff = 1
+    elif direction == 'U':
+        y_diff = 1
+    elif direction == 'D':
+        y_diff = -1
+
+    return x_diff, y_diff
 
 
 def find_intersections(wire1, wire2):
@@ -56,6 +63,6 @@ def calculate_min_distance(intersections):
 
 
 if __name__ == "__main__":
-    data = parse_data('input.txt')
-    result = manhattan_distance(data[0], data[1])
+    wire1, wire2 = parse_data('input.txt')
+    result = manhattan_distance(wire1, wire2)
     print("Result: " + str(result))  # 18542
